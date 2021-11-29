de118434776c71f8ac37221bbfbef6396cb56b49
Danielyan86 <516495459@qq.com>
2021-11-29 16:42:28 +0800

    update

--- cypress/integration/first_test.js
+++ cypress/integration/first_test.js
@@ -6,11 +6,11 @@
 // https://on.cypress.io/writing-first-test
 describe('My First Test', () => {
 	it('Does not do much!', () => {
-		expect(true).to.equal(false)
+		expect(true).to.equal(true)
 	})
 	it('Visits the Kitchen Sink', () => {
 		cy.visit('https://example.cypress.io')
 		cy.contains('type').click()
-		cy.url().should('include','/commands/actions')
+		cy.url().should('include', '/commands/actions')
 	})
 })
